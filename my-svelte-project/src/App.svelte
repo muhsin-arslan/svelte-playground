25345ecc26ad6bb9a15edb867c22f5573449e183
Muhsin ARSLAN <developermuhsin@gmail.com>
2021-01-04 17:55:36 +0300

    Update

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let count;
+	$: doubled = count * 2;
+
+	$: if (count >= 10) {
+		alert("Count access the limit!");
+		count = 9;
+	}
+
+	function handleClick() {
+		count++;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked
+	{count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
